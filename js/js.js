b6b56afe5619b2d1662424cca9f35d445ba2c90b
Cameron Sahagian-Crandall <ccrand@umich.edu>
2023-03-15 02:20:02 -0400

    updated slider and added up to top button

--- js/js.js
+++ js/js.js
@@ -5,10 +5,38 @@ function changeSize(element) {
     console.log(imgs.length)
     for (var index = 0; index < imgs.length; index += 1) {
         console.log(index)
-        imgs[index].style.width = element.value+"%"
+        if (imgs[index].id == "portrait") {
+            imgs[index].style.width = element.value+"%"
+        }
+        if (imgs[index].id == "landscape") {
+            imgs[index].style.width = 1.6*element.value+"%"
+        }
     }
 }
 
 function test() {
     console.log("Working!");
+}
+
+// totop button
+let totop = document.getElementById("totop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    totop.style.display = "block";
+  } else {
+    totop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+//   document.body.scrollTop = 0;
+  document.body.scrollIntoView({behavior: 'smooth', block: 'start'});
+  document.documentElement.scrollIntoView({behavior: 'smooth', block: 'start'});
 }
