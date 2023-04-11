405b428ecce6c08fceb3af237452c9f88c73de82
Cameron Sahagian-Crandall <ccrand@umich.edu>
2023-04-11 16:17:07 -0400

    Changed a lot. Added lightbox for Pictures page, updated js.js so color scheme loads right away.

--- js/js.js
+++ js/js.js
@@ -1,103 +1,3 @@
-//images
-var imgs = document.querySelectorAll("img")
-
-// totop button
-var totop = document.querySelector("#totop")
-
-function changeSize(element) {
-    console.log("Don't slide away!")
-    console.log(element.value)
-    console.log(imgs.length)
-    for (var index = 0; index < imgs.length; index += 1) {
-        console.log(imgs[index])
-        if (imgs[index].classList.contains("portrait")) {
-            imgs[index].style.width = element.value+"%"
-        }
-        if (imgs[index].classList.contains("landscape")) {
-            imgs[index].style.width = 1.6*element.value+"%"
-        }
-    }
-}
-
-function test() {
-    console.log("Working!");
-}
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    totop.style.display = "block";
-  } else {
-    totop.style.display = "none";
-  }
-}
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-    scrollFunction()
-};
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollIntoView({behavior: 'smooth', block: 'start'});
-  document.documentElement.scrollIntoView({behavior: 'smooth', block: 'start'});
-}
-
-function show_all() {
-    console.log("working!");
-    for (var index = 0; index < imgs.length; index += 1) {
-        console.log(imgs[index])
-        imgs[index].style.removeProperty('display')
-    }
-}
-
-function show_headshot() {
-    for (var index = 0; index < imgs.length; index += 1) {
-        if (imgs[index].classList.contains("headshot")) {
-            console.log(imgs[index])
-            imgs[index].style.removeProperty('display')
-        }
-        else { 
-            imgs[index].style.display = "none"
-        }
-    }
-}
-
-function show_selfie() {
-    for (var index = 0; index < imgs.length; index += 1) {
-        if (imgs[index].classList.contains("selfie")) {
-            console.log(imgs[index])
-            imgs[index].style.removeProperty('display')
-        }
-        else { 
-            imgs[index].style.display = "none"
-        }
-    }
-}
-
-function show_friends() {
-    for (var index = 0; index < imgs.length; index += 1) {
-        if (imgs[index].classList.contains("friends")) {
-            console.log(imgs[index])
-            imgs[index].style.removeProperty('display')
-        }
-        else { 
-            imgs[index].style.display = "none"
-        }
-    }
-}
-
-function show_family() {
-    for (var index = 0; index < imgs.length; index += 1) {
-        if (imgs[index].classList.contains("family")) {
-            console.log(imgs[index])
-            imgs[index].style.removeProperty('display')
-        }
-        else { 
-            imgs[index].style.display = "none"
-        }
-    }
-}
-
 // Change Color Theme
 function changeTheme() {
     console.log("Color scheme changed!")
@@ -240,6 +140,116 @@ function loadTheme() {
 }
 
 loadTheme()
+
+// totop button
+var totop = document.querySelector("#totop")
+
+function changeSize(element) {
+    console.log("Don't slide away!")
+    console.log(element.value)
+    console.log(imgs.length)
+    for (var index = 0; index < imgs.length; index += 1) {
+        console.log(imgs[index])
+        if (imgs[index].classList.contains("portrait")) {
+            imgs[index].style.width = element.value+"%"
+        }
+        if (imgs[index].classList.contains("landscape")) {
+            imgs[index].style.width = 1.6*element.value+"%"
+        }
+    }
+}
+
+function test() {
+    console.log("Working!");
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    totop.style.display = "block";
+  } else {
+    totop.style.display = "none";
+  }
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction()
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollIntoView({behavior: 'smooth', block: 'start'});
+  document.documentElement.scrollIntoView({behavior: 'smooth', block: 'start'});
+}
+
+// Pictures Page
+var imgs = document.querySelectorAll(".gallery a")
+
+function show_all() {
+    console.log("working!");
+    for (var index = 0; index < imgs.length; index += 1) {
+        console.log(imgs[index])
+        imgs[index].style.removeProperty('display')
+        imgs[index].removeAttribute('data-lightbox')
+        imgs[index].setAttribute('data-lightbox', 'All')
+}
+}
+
+function show_headshot() {
+    for (var index = 0; index < imgs.length; index += 1) {
+        if (imgs[index].classList.contains("headshot")) {
+            console.log(imgs[index])
+            imgs[index].style.removeProperty('display')
+            imgs[index].removeAttribute('data-lightbox')
+            imgs[index].setAttribute('data-lightbox', 'headshot')
+        }
+        else { 
+            imgs[index].style.display = "none"
+        }
+    }
+}
+
+function show_selfie() {
+    for (var index = 0; index < imgs.length; index += 1) {
+        if (imgs[index].classList.contains("selfie")) {
+            console.log(imgs[index])
+            imgs[index].style.removeProperty('display')
+            imgs[index].removeAttribute('data-lightbox')
+            imgs[index].setAttribute('data-lightbox', 'selfie')
+        }
+        else { 
+            imgs[index].style.display = "none"
+        }
+    }
+}
+
+function show_friends() {
+    for (var index = 0; index < imgs.length; index += 1) {
+        if (imgs[index].classList.contains("friends")) {
+            console.log(imgs[index])
+            imgs[index].style.removeProperty('display')
+            imgs[index].removeAttribute('data-lightbox')
+            imgs[index].setAttribute('data-lightbox', 'friends')
+        }
+        else { 
+            imgs[index].style.display = "none"
+        }
+    }
+}
+
+function show_family() {
+    for (var index = 0; index < imgs.length; index += 1) {
+        if (imgs[index].classList.contains("family")) {
+            console.log(imgs[index])
+            imgs[index].style.removeProperty('display')
+            imgs[index].removeAttribute('data-lightbox')
+            imgs[index].setAttribute('data-lightbox', 'family')
+        }
+        else { 
+            imgs[index].style.display = "none"
+        }
+    }
+}
 
 //Songs Page
 
